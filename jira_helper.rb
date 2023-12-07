a63fd1cd0d01be3105577269920c7d630ae0c678
Ger O'Mahony <gomahony@kitmanlabs.com>
2023-12-07 12:13:46 +0000

    Initial test code

---
+++ jira_helper.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'pry'
+require 'net/http'
+require 'json'
+
+JIRA_API_KITMANLABS = 'https://kitmanlabs.atlassian.net/rest/api/3/'
+
+def jira_username
+  `op read 'op://Private/Jira API Token/username'`.chop
+end
+
+def jira_token
+  `op read 'op://Private/Jira API Token/password'`.chop
+end
+
+def get_request(uri_string:)
+  uri = URI(uri_string)
+  request = Net::HTTP::Get.new(uri)
+
+  request.basic_auth(jira_username, jira_token)
+  req_options = { use_ssl: uri.scheme == 'https' }
+  Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request)
+  end
+end
+
+def post_request(uri_string:, data:)
+  uri = URI(uri_string)
+  request = Net::HTTP::Post.new(uri)
+
+  request.basic_auth(jira_username, jira_token)
+  req_options = { use_ssl: uri.scheme == 'https' }
+  Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+    http.request(request, data)
+  end
+end
+
+uri_string = JIRA_API_KITMANLABS + 'search?jql=text%20~%20"FlakySpec*"%20AND%20Labels=FlakySpec'
+response = get_request(uri_string: uri_string)
+body = JSON.parse(response.body)
+binding.pry
